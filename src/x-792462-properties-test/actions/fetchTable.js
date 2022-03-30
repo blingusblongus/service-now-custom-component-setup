a03c9c0aabcd407cd8ff37bbc4059a237e298df3
Nick Allen <nallen@esolutionsone.com>
2022-03-30 09:36:40 -0500

    refactored

---
+++ src/x-792462-properties-test/actions/fetchTable.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+//////////////////// GET TABLE
+// the function we want to comprise the effect
+async function httpEffect(url, options, coeffects) {
+	const { dispatch, properties } = coeffects;
+	const { tableName, limit, fields, queries } = properties;
+    console.log(properties)
+
+	// build REST params
+	url += `${tableName}?sysparm_limit=${limit}&sysparm_fields=${fields}`;
+
+	let queryArr = queries.split(',');
+	if (queries[0] !== '') {
+		for (let query of queryArr) {
+			url += `&${query}`;
+		}
+	}
+
+	dispatch('FETCH_STARTED');
+	try {
+		const result = await axios.get(url)
+		dispatch('FETCH_SUCCEEDED', result);
+	} catch (e) {
+		dispatch('FETCH_FAILED', e, {}, true)
+        console.log(e);
+	}
+}
+
+// the effect itself
+function createHttpEffect(url, options) {
+	return {
+		effect: httpEffect,
+		args: [url, options]
+	}
+}
+
+export const fetchTableEffect = createHttpEffect('api/now/table/');
+
+// handler for fetch success
+export const handleFetchTableSucceeded = ({ action }) => console.log(action.payload);
+
+// handler for fetch fail
+export const handleFetchTableFailed = ({ action }) => console.log('User fetch failed!')
--- src/x-792462-properties-test/index.js
+++ src/x-792462-properties-test/index.js
@@ -1,58 +1,15 @@
 import { createCustomElement, actionTypes } from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
-import axios from 'axios';
 import styles from './styles.scss';
 // import { createHttpEffect } from '@servicenow/ui-effect-http';
 import view from './src/view';
+import { 
+	fetchTableEffect, 
+	handleFetchTableSucceeded, 
+	handleFetchTableFailed 
+} from './actions/fetchTable';
 
-
-const USER_FETCHED = 'USER_FETCHED';
-const {COMPONENT_RENDERED} = actionTypes;
-
-//////////////////// GET TABLE
-// the function we want to comprise the effect
-async function httpEffect(url, options, coeffects) {
-	const { dispatch, properties } = coeffects;
-	const {tableName, limit, fields, queries} = properties;
-
-	// build REST params
-	url += `${tableName}?sysparm_limit=${limit}&sysparm_fields=${fields}`;
-
-	let queryArr = queries.split(',');
-	if(queries[0] !== ''){
-		for(let query of queryArr){
-			url += `&${query}`;
-		}
-	}
-
-	dispatch('FETCH_STARTED');
-	try {
-		const result = await axios.get(url, {
-				// headers: {
-				// 	"Authorization": "Basic YWRtaW46ZGFoNGVGMkRUWUJ0",
-				// }
-			})
-		dispatch('FETCH_SUCCEEDED', result);
-	}catch(e){
-		dispatch('FETCH_FAILED', e, {}, true)
-	}
-}
-
-// the effect itself
-function createHttpEffect(url, options) {
-	return {
-		effect: httpEffect,
-		args: [url, options]
-	}
-}
-
-const fetchUserEffect = createHttpEffect('api/now/table/');
-
-// handler for fetch success
-const handleFetchUserSucceeded = ({action}) => console.log(action.payload);
-
-// handler for fetch fail
-const handleFetchUserFailed = ({action}) => console.log('User fetch failed!')
+const { COMPONENT_RENDERED } = actionTypes;
 
 createCustomElement('x-792462-properties-test', {
 	renderer: { type: snabbdom },
@@ -62,21 +19,21 @@ createCustomElement('x-792462-properties-test', {
 		userName: { default: 'default user' },
 		tableName: { default: 'sys_user' },
 		fields: { default: '' },
-		limit: { default: '10'},
-		queries: { default: 'active=true'}
+		limit: { default: '10' },
+		queries: { default: 'active=true' }
 	},
 	actionHandlers: {
 		// dispatched within component view or COMPONENT_CONNECTED action handler
-		[COMPONENT_RENDERED]: fetchUserEffect,
-		// ['USER_FETCHED']: fetchUserEffect,
+		[COMPONENT_RENDERED]: fetchTableEffect,
+		// ['USER_FETCHED']: fetchTableEffect,
 		// success
-		FETCH_SUCCEEDED: ({action, state, updateState}) => {
-			handleFetchUserSucceeded({action});
+		FETCH_SUCCEEDED: ({ action, state, updateState }) => {
+			handleFetchTableSucceeded({ action });
 			// this is probably not ideal but it seems to work
-			if(state.tableData) return;
-			updateState({tableData: action.payload.data.result});
+			if (state.tableData) return;
+			updateState({ tableData: action.payload.data.result });
 		},
 		// fail
-		FETCH_FAILED: handleFetchUserFailed,
+		FETCH_FAILED: handleFetchTableFailed,
 	}
 });
