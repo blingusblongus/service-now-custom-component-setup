dc565dd469ee59048f4204d4ebb9aafc6e6bd45e
Nick Allen <nallen@esolutionsone.com>
2022-03-31 11:37:25 -0500

    comment in simple API using proxy

--- src/x-792462-properties-test/src/view.js
+++ src/x-792462-properties-test/src/view.js
@@ -2,7 +2,19 @@ import TableComponent from './TableComponent/TableComponent';
 const view = (state, { updateState, dispatch }) => {
 	const { properties, tableData } = state;
 	const {fields} = properties;
-	
+
+    /* 
+    ALTERNATE METHOD TO USING COMPONENT_RENDERED ACTION HANDLER & createHttpRequest()
+    Requires proxy set and logged in to CLI
+    
+    axios.get(`api/now/table/sys_user?sysparm_limit=10&sysparm_fields=first_name`)
+        .then(response => {
+            if(state.tableData) return;
+            updateState({tableData: response.data.result})
+        })
+        .catch(err => console.log(err));
+	*/
+    
 	return (
 		<view>
 			<div className="text-center">Hello {properties.userName}</div>	
